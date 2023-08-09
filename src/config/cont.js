a287ef9301a914541e389403d1c7412602ba3ce8
Audrey Hempel <audrey.hempel@yale.edu>
2023-08-09 12:41:12 -0700

    2x3 orderfiles and upstream changes

--- src/config/cont.js
+++ src/config/cont.js
@@ -16,6 +16,7 @@
 //                       that returns the value
 //                       changed trial_stim defining into a function
 //                       that returns the value
+//        8/9/23  (AGH): added and incorporated new 2x3 orderfiles
 //
 //   --------------------
 //   This file includes the return functions writeOrderfile and loadOrderfile 
@@ -31,26 +32,32 @@
 // import { orderfile } from '../components/Login';
 
 // importing each unique trial stimulus sets
-import { ts1_1_1 } from './jsOrders/cMST_Imbal2_orders_1_1_1';
-import { ts1_2_1 } from './jsOrders/cMST_Imbal2_orders_1_2_1';
-import { ts2_1_1 } from './jsOrders/cMST_Imbal2_orders_2_1_1';
-import { ts2_2_1 } from './jsOrders/cMST_Imbal2_orders_2_2_1';
-import { ts3_1_1 } from './jsOrders/cMST_Imbal2_orders_3_1_1';
-import { ts3_2_1 } from './jsOrders/cMST_Imbal2_orders_3_2_1';
-import { ts4_1_1 } from './jsOrders/cMST_Imbal2_orders_4_1_1';
-import { ts4_2_1 } from './jsOrders/cMST_Imbal2_orders_4_2_1';
-import { ts5_1_1 } from './jsOrders/cMST_Imbal2_orders_5_1_1';
-import { ts5_2_1 } from './jsOrders/cMST_Imbal2_orders_5_2_1';
-import { ts6_1_1 } from './jsOrders/cMST_Imbal2_orders_6_1_1';
-import { ts6_2_1 } from './jsOrders/cMST_Imbal2_orders_6_2_1';
+import { ts2x3_1_1_1 } from './jsOrders/cMST_Imbal2x3_orders_1_1_1';
+import { ts2x3_1_1_2 } from './jsOrders/cMST_Imbal2x3_orders_1_1_2';
+import { ts2x3_1_1_3 } from './jsOrders/cMST_Imbal2x3_orders_1_1_3';
+import { ts2x3_2_1_1 } from './jsOrders/cMST_Imbal2x3_orders_2_1_1';
+import { ts2x3_2_1_2 } from './jsOrders/cMST_Imbal2x3_orders_2_1_2';
+import { ts2x3_2_1_3 } from './jsOrders/cMST_Imbal2x3_orders_2_1_3';
+import { ts2x3_3_1_1 } from './jsOrders/cMST_Imbal2x3_orders_3_1_1';
+import { ts2x3_3_1_2 } from './jsOrders/cMST_Imbal2x3_orders_3_1_2';
+import { ts2x3_3_1_3 } from './jsOrders/cMST_Imbal2x3_orders_3_1_3';
+import { ts2x3_4_1_1 } from './jsOrders/cMST_Imbal2x3_orders_4_1_1';
+import { ts2x3_4_1_2 } from './jsOrders/cMST_Imbal2x3_orders_4_1_2';
+import { ts2x3_4_1_3 } from './jsOrders/cMST_Imbal2x3_orders_4_1_3';
+import { ts2x3_5_1_1 } from './jsOrders/cMST_Imbal2x3_orders_5_1_1';
+import { ts2x3_5_1_2 } from './jsOrders/cMST_Imbal2x3_orders_5_1_2';
+import { ts2x3_5_1_3 } from './jsOrders/cMST_Imbal2x3_orders_5_1_3';
+import { ts2x3_6_1_1 } from './jsOrders/cMST_Imbal2x3_orders_6_1_1';
+import { ts2x3_6_1_2 } from './jsOrders/cMST_Imbal2x3_orders_6_1_2';
+import { ts2x3_6_1_3 } from './jsOrders/cMST_Imbal2x3_orders_6_1_3';
 
 //----------------------- 2 ----------------------
 //------------------- FUNCTIONS ------------------
 
 // write the orderfile path depending on selected set, trialorder and run options
-function writeOrderfile(stim_set, trialorder, run) {
-  var orderprefix = './jsOrders/cMST_Imbal2_orders_';
-  var orderfile = orderprefix + stim_set + '_' + trialorder + '_' + run 
+function writeOrderfile(stim_set, sublist) {
+  var orderprefix = './jsOrders/cMST_Imbal2x3_orders_';
+  var orderfile = orderprefix + stim_set + '_1_' + sublist; 
   console.log('orderfile = ' + orderfile);
   return orderfile;
 }
@@ -60,43 +67,61 @@ function loadOrderfile(orderfile) {
 
   var trial_stim;
 
-  if (orderfile == './jsOrders/cMST_Imbal2_orders_1_1_1') {
-    return trial_stim = ts1_1_1;
+  if (orderfile == './jsOrders/cMST_Imbal2x3_orders_1_1_1') {
+    return trial_stim = ts2x3_1_1_1;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_1_2_1') {
-    return trial_stim = ts1_2_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_1_1_2') {
+    return trial_stim = ts2x3_1_1_2;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_2_1_1') {
-    return trial_stim = ts2_1_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_1_1_3') {
+    return trial_stim = ts2x3_1_1_3;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_2_2_1') {
-    return trial_stim = ts2_2_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_2_1_1') {
+    return trial_stim = ts2x3_2_1_1;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_3_1_1') {
-    return trial_stim = ts3_1_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_2_1_2') {
+    return trial_stim = ts2x3_2_1_2;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_3_2_1') {
-    return trial_stim = ts3_2_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_2_1_3') {
+    return trial_stim = ts2x3_2_1_3;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_4_1_1') {
-    return trial_stim = ts4_1_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_3_1_1') {
+    return trial_stim = ts2x3_3_1_1;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_4_2_1') {
-    return trial_stim = ts4_2_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_3_1_2') {
+    return trial_stim = ts2x3_3_1_2;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_5_1_1') {
-    return trial_stim = ts5_1_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_3_1_3') {
+    return trial_stim = ts2x3_3_1_3;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_5_2_1') {
-    return trial_stim = ts5_2_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_4_1_1') {
+    return trial_stim = ts2x3_4_1_1;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_6_1_1') {
-    return trial_stim = ts6_1_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_4_1_2') {
+    return trial_stim = ts2x3_4_1_2;
   } 
-  else if (orderfile == './jsOrders/cMST_Imbal2_orders_6_2_1') {
-    return trial_stim = ts6_2_1;
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_4_1_3') {
+    return trial_stim = ts2x3_4_1_3;
+  } 
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_5_1_1') {
+    return trial_stim = ts2x3_5_1_1;
+  } 
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_5_1_2') {
+    return trial_stim = ts2x3_5_1_2;
+  } 
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_5_1_3') {
+    return trial_stim = ts2x3_5_1_3;
+  } 
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_6_1_1') {
+    return trial_stim = ts2x3_6_1_1;
+  } 
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_6_1_2') {
+    return trial_stim = ts2x3_6_1_2;
+  } 
+  else if (orderfile == './jsOrders/cMST_Imbal2x3_orders_6_1_3') {
+    return trial_stim = ts2x3_6_1_3;
   }
-}
+} 
 
 // //----------------------- 3 ----------------------
 // //-------------------- EXPORTS -------------------
